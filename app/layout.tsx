2d041b6ba39e07acbcd883799ef791026b259633
Simon-Pierre Boucher <35044110+simonpierreboucher@users.noreply.github.com>
2024-12-10 17:11:24 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'Searchy By M-LAI'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  ''
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://morphic.sh'),
