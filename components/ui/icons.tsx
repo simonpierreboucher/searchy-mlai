c2a1df8894cae5c3c131a300501489ff7074e479
Simon-Pierre Boucher <35044110+simonpierreboucher@users.noreply.github.com>
2024-12-10 18:00:29 -0500

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -2,20 +2,14 @@
 
 import { cn } from '@/lib/utils'
 
-function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
+function IconLogo({ className, ...props }: React.ComponentProps<'img'>) {
   return (
-    <svg
-      fill="currentColor"
-      viewBox="0 0 256 256"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
+    <img
+      src="https://raw.githubusercontent.com/M-Lai-ai/logo/refs/heads/main/favicon.ico"
+      alt="Logo"
       className={cn('h-4 w-4', className)}
       {...props}
-    >
-      <circle cx="128" cy="128" r="128" fill="black"></circle>
-      <circle cx="102" cy="128" r="18" fill="white"></circle>
-      <circle cx="154" cy="128" r="18" fill="white"></circle>
-    </svg>
+    />
   )
 }
 
