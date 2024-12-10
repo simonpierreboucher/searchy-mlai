b44b93d2a51f92cd948d323d42dfd71ecb38a8f1
Simon-Pierre Boucher <35044110+simonpierreboucher@users.noreply.github.com>
2024-12-10 18:08:46 -0500

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -5,28 +5,16 @@ import { cn } from '@/lib/utils'
 function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
     <svg
+      fill="currentColor"
       viewBox="0 0 256 256"
       role="img"
       xmlns="http://www.w3.org/2000/svg"
-      className={cn('h-8 w-8', className)} // Taille augmentée pour meilleure lisibilité
+      className={cn('h-4 w-4', className)}
       {...props}
     >
-      {/* Cercle de fond */}
-      <circle cx="128" cy="128" r="128" fill="#000000" /> {/* Noir de fond */}
-
-      {/* Texte "M-LAI" centré */}
-      <text
-        x="50%"
-        y="50%"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fill="#FFFFFF" // Couleur du texte en blanc
-        fontSize="64" // Taille du texte ajustée
-        fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif" // Police de caractères
-        fontWeight="bold" // Texte en gras pour meilleure visibilité
-      >
-        M-LAI
-      </text>
+      <circle cx="128" cy="128" r="128" fill="black"></circle>
+      <circle cx="102" cy="128" r="18" fill="white"></circle>
+      <circle cx="154" cy="128" r="18" fill="white"></circle>
     </svg>
   )
 }
