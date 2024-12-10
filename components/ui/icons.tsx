b90a8462e1eeb864190f4a268825599308cae447
Simon-Pierre Boucher <35044110+simonpierreboucher@users.noreply.github.com>
2024-12-10 18:02:23 -0500

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import Image from 'next/image'
 import { cn } from '@/lib/utils'
 
-function IconLogo({ className, ...props }: React.ComponentProps<'img'>) {
+function IconLogo({ className, ...props }: React.ComponentProps<typeof Image>) {
   return (
-    <img
+    <Image
       src="https://raw.githubusercontent.com/M-Lai-ai/logo/refs/heads/main/favicon.ico"
       alt="Logo"
       className={cn('h-4 w-4', className)}
+      width={16}
+      height={16}
       {...props}
     />
   )
